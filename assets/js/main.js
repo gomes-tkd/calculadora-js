8508ac968e5226ec6f0678fd4615a91903bdf3eb
José Gomes <106411184+gomes-tkd@users.noreply.github.com>
2022-12-27 00:10:56 -0300

    Limpando o codigo pt. 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,9 @@
 const pResult = document.querySelector(".pResult");
-const label = document.querySelector(".label");
-const btnEqual = document.querySelector(".btnOppEqual").innerHTML;
-
-console.log(btnEqual.innerHTML);
 
 function numero(num){
     let numero = pResult.innerHTML;
     pResult.innerHTML = numero + num;
 }
-
 
 function result() {
     pResult.innerHTML = eval(pResult.innerHTML);
@@ -17,30 +12,3 @@ function result() {
 function clearResults() {
     pResult.innerHTML = "";
 }
-
-/*
-label.addEventListener("click", (e) => {
-    
-    let op = e.target.innerHTML.toString();
-    switch (op) {
-        case "+":
-            console.log("+");
-            break;
-        case "-":
-            console.log("-");
-            break;
-        case "*":
-            console.log("*");
-            break;
-        case "/":
-            console.log("/");
-            break;
-        case "=":
-            break;
-        default:
-            break;
-    }
-
-    pResult.innerHTML = e.target.innerHTML;
-
-});*/
